1af013f23ccb30d9116bf23195f4078535d783fb
Vishak Nambiar <intrinsic88@outlook.com>
2020-08-24 22:29:54 +0530

    Changes for react

---
+++ build/precache-manifest.88e47248905117c2e34bc4da90299cf8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80c465468caa649b101693897ab569a6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1c0cb619ff7d7a9eb1b0",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2a783f8387048414e1df",
+    "url": "/static/js/2.736c39bd.chunk.js"
+  },
+  {
+    "revision": "7e7c0a2ab3d68b6cfe081998a76231d0",
+    "url": "/static/js/2.736c39bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c0cb619ff7d7a9eb1b0",
+    "url": "/static/js/main.bfe71237.chunk.js"
+  },
+  {
+    "revision": "f36726a44e56e4b74f8f",
+    "url": "/static/js/runtime-main.bd717dcb.js"
+  }
+]);
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,60 @@
 var express = require('express');
 var router = express.Router();
+var jsonfile = require('jsonfile')
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+    res.send('respond with a resource');
 });
+
+
+/******************************************************************************
+ *                       Login - "POST /api/users/login"
+ ******************************************************************************/
+
+
+const openDb = async() => {
+    const dbFilePath = './public/MockDb.json';
+
+    return jsonfile.readFile(dbFilePath);
+}
+
+async function login(email, password) {
+
+    try {
+        const db = await openDb();
+        for (const user of db.users) {
+            if (user.email === email && user.password === password) {
+                return user;
+            }
+        }
+        return null;
+    } catch (err) {
+        throw err;
+    }
+}
+
+router.post('/login', async(req, res) => {
+    const { users } = req.body;
+    if (!users) {
+        return res.status(BAD_REQUEST).json({
+            error: paramMissingError,
+        });
+    }
+    const logedinUser = await login(users.email, users.password);
+    if (logedinUser)
+        return res.status(200).end();
+    else
+        return res.status(400).end();
+});
+
+
+router.get('/all', async(req, res) => {
+
+    const db = await openDb();
+    console.log(db.users)
+    return res.status(200).json(db);
+});
+
 
 module.exports = router;
